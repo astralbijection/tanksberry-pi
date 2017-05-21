824916d83aa5a1af6550697fd309c7a9af62c294
Astrid Yu <astrid@astrid.tech>
2017-05-21 10:41:48 -0700

    Allow user to set control scheme

--- static/js/movement.js
+++ static/js/movement.js
@@ -1,7 +1,7 @@
 /* global $ */
 
 var drive = {
-	mode: 'tank',
+	mode: 'none',
 	power: 0,
 	left: 0,
 	right: 0,
@@ -11,54 +11,67 @@ var turret = {
 
 };
 
+function getDriveMode() {
+	return $('#driveconfig input[name=drivemode]:checked').val();
+}
+
 $(document).keydown(function(event) {
-	/*
-	switch (event.key) {
-	case 'w':
-		drive.power = 1;
+
+	switch (getDriveMode()) {
+	case 'wasd':
+		switch (event.key) {
+		case 'w':
+			drive.power = 1;
+			break;
+		case 's':
+			drive.power = -1;
+			break;
+		case 'a':
+			drive.turn = 'left';
+			break;
+		case 'd':
+			drive.turn = 'right';
+			break;
+		}
 		break;
-	case 's':
-		drive.power = -1;
-		break;
-	case 'a':
-		drive.turn = 'left';
-		break;
-	case 'd':
-		drive.turn = 'right';
-		break;
-	}
-	*/
-	switch (event.key) {
-	case 'q':
-		drive.left = 1;
-		break;
-	case 'a':
-		drive.left = -1;
-		break;
-	case 'e':
-		drive.right = 1;
-		break;
-	case 'd':
-		drive.right = -1;
+	case 'tank':
+		switch (event.key) {
+		case 'q':
+			drive.left = 1;
+			break;
+		case 'a':
+			drive.left = -1;
+			break;
+		case 'e':
+			drive.right = 1;
+			break;
+		case 'd':
+			drive.right = -1;
+			break;
+		}
 		break;
 	}
 }).keyup(function(event) {
-	/*
-	switch (event.key) {
-	case 'w': case 's':
-		drive.power = 0;
+	switch (getDriveMode()) {
+	case 'wasd':
+		switch (event.key) {
+		case 'w': case 's':
+			drive.power = 0;
+			break;
+		case 'a': case 'd':
+			drive.turn = 'none'
+			break;
+		}
 		break;
-	case 'a': case 'd':
-		drive.turn = 'none'
-		break;
-	}
-	*/
-	switch(event.key) {
-	case 'q': case 'a':
-		drive.left = 0;
-		break;
-	case 'e': case 'd':
-		drive.right = 0;
+	case 'tank':
+		switch(event.key) {
+		case 'q': case 'a':
+			drive.left = 0;
+			break;
+		case 'e': case 'd':
+			drive.right = 0;
+			break;
+		}
 		break;
 	}
 });
@@ -70,8 +83,9 @@ var socket;
 $(function() {
 	socket = new WebSocket('ws://' + window.location.hostname + ':8081');
 	socket.onopen = function() {
+		console.log('connected');
 		setInterval(function() {
-			socket.send(JSON.stringify({drive: drive, turret: turret}));
+			socket.send(JSON.stringify({drive: getDriveMode(), turret: turret}));
 		}, 50);
 	}
 });
